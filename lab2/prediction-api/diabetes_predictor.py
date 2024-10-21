b2f831fb68779ba4330da773e7357b23ba2418fc
newnieuws <nielsb2000.nb@gmail.com>
2024-10-21 13:02:05 +0200

    eerste echt commit

--- lab2/prediction-api/diabetes_predictor.py
+++ lab2/prediction-api/diabetes_predictor.py
@@ -1,12 +1,12 @@
 import json
 import os
-
+import pickle
 import pandas as pd
 from flask import jsonify
 from keras.models import load_model
 import logging
 from io import StringIO
-
+from sklearn.preprocessing import StandardScaler
 
 class DiabetesPredictor:
     def __init__(self):
@@ -24,9 +24,11 @@ class DiabetesPredictor:
                 self.model = load_model('model.h5')
 
         df = pd.read_json(StringIO(json.dumps(prediction_input)), orient='records')
-        y_pred = self.model.predict(df)
+        scaler = StandardScaler()
+        test_instance_scaled = scaler.transform(df)
+        y_pred = self.model.predict(test_instance_scaled)
         logging.info(y_pred[0])
-        status = (y_pred[0] > 0.5)
+        status = (y_pred[0] > 5)
         logging.info(type(status[0]))
         # return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
         return jsonify({'result': str(status[0])}), 200
